186ec25e2748056a766f6cc218fe1a0a3eeb52f9
Ixf <ixf2@outlook.es>
2025-12-07 15:23:45 +0000

    feat: Ventana de chat con firebase

--- js/gotham-menu.js
+++ js/gotham-menu.js
@@ -2,3 +2,18 @@ function toggleMenu() {
   const MENU_LINKS = document.querySelector('.menu-links');
   MENU_LINKS.classList.toggle('show');
 }
+
+//MESSAGES CHAT MENU
+const CHAT_ICON = document.getElementById("chat-icon");
+const CHAT_CONTAINER = document.getElementById("chat-container");
+const CLOSE_CHAT = document.querySelector(".close-chat");
+
+CHAT_ICON.addEventListener("click", () => {
+  CHAT_CONTAINER.style.display = "flex";
+  CHAT_ICON.style.display = "none"; // ocultar el icono
+});
+
+CLOSE_CHAT.addEventListener("click", () => {
+  CHAT_CONTAINER.style.display = "none";
+  CHAT_ICON.style.display = "flex"; // volver a mostrar el icono
+});
